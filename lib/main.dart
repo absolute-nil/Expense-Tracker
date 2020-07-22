07e93059d7999c91e5015885d05a2f863d2251bd
Nikhil Sharma <nikhil19@somaiya.edu>
2020-07-23 03:13:37 +0530

    displat transactions and basic styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,70 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Laptop', amount: 100000.50, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Logitech Mouse', amount: 8000, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-      appBar: AppBar(
-        title: Text("Expense Manager"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: Card(
-              child: Text("Chart"),
-              elevation: 5,
-              color: Colors.blue,
+        appBar: AppBar(
+          title: Text("Expense Manager"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                child: Text("Chart"),
+                elevation: 5,
+                color: Colors.blue,
+              ),
+              width: double.infinity,
             ),
-            width: double.infinity,
-          ),
-          Card(
-            child: Text("List of expenses"),
-            color: Colors.red,
-          )
-        ],
+            Column(
+                children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                            color: Colors.purple,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Text(tx.title,style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                          Text(tx.date.toString(), style: TextStyle(fontSize: 10,color: Colors.grey),)
+                        ],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }).toList())
+          ],
+        ),
       ),
-    ),
     );
-    
   }
 }
