e4e0f0da37d984cffb8a01dd8786ff1a43e7bd43
Nikhil Sharma <nikhil19@somaiya.edu>
2020-07-24 02:18:48 +0530

    convert to statefull to persist form data

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function addTransaction;
 
   NewTransaction(this.addTransaction);
 
-  void submitTransaction(){
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
+
+  void submitTransaction() {
     final title = titleController.text;
     final amount = double.parse(amountController.text);
-    if(title.isEmpty || amount <= 0){
+    if (title.isEmpty || amount <= 0) {
       return;
     }
-    addTransaction(title, amount);
+    widget.addTransaction(title, amount);
+
+    Navigator.of(context).pop();
   }
+
   @override
   Widget build(BuildContext context) {
     return Card(
